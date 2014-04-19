ac47c94079ee524775216bc30d2e3cae493681e8
underscorephil <underscorephil@gmail.com>
2014-04-19 11:12:09 -0500

    Init commit

---
+++ 2do.py
@@ -0,0 +1,56 @@
+
+import sys
+import re
+from workflow import Workflow, ICON_WEB, ICON_WARNING
+import argparse
+
+
+def main(wf):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('query', nargs='?')
+    parser.add_argument('--default-list', dest='default_list', nargs='?')
+    args = parser.parse_args(wf.args)
+
+    # get query from alfred
+    query = args.query
+
+    tags = query
+    tags = {tag.strip("#") for tag in tags.split() if tag.startswith("#")}
+    task_title = re.sub(r"(?:\#|:)[A-Za-z]+", "", query).strip()
+
+    list = re.search('\s:[A-Z][a-z]+$', query)
+    if list:
+        list = list.group(0).replace(':', '').strip()
+
+    # save default list
+    if args.default_list:
+        wf.settings['default_list'] = args.default_list
+        wf.add_item('Set list', 'list', valid=False)
+        return 0
+
+    # check default list
+    if not list:
+        default_list = wf.settings.get('default_list', None)
+        if not default_list:
+            wf.add_item('No Default List Set.',
+                        'Please use defaultlist to set the default list\
+                         or specify a list',
+                        valid=False,
+                        icon=ICON_WARNING)
+            wf.send_feedback()
+            return 0
+        else:
+            list = default_list
+
+    url = 'twodo:///add?task=%s&forlist=%s' % (task_title, list)
+    if len(tags):
+        url += '&tags=%s' % (",".join(tags))
+    # Send the results to Alfred as XML
+    alfred_title = 'Add task: %s' % (task_title)
+    wf.add_item(title=alfred_title, arg=url, valid=True)
+    wf.send_feedback()
+
+
+if __name__ == u"__main__":
+    wf = Workflow()
+    sys.exit(wf.run(main))
